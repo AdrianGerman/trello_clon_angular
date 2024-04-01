06fa07157d5efe336cf5e6d43aeef9435058313f
AdrianGerman <adrian.german1019@gmail.com>
2024-04-01 10:18:16 -0600

    overlays

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { BtnComponent } from '../../components/btn/btn.component';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [OverlayModule, BtnComponent],
+  templateUrl: './navbar.component.html',
+})
+export class NavbarComponent {
+  isOpen = false;
+}
