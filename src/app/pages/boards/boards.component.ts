c9215f55c5a1b528f468b2f4ab1a17f293df14ee
AdrianGerman <adrian.german1019@gmail.com>
2024-04-01 13:45:26 -0600

    accordions

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,17 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faTrello } from '@fortawesome/free-brands-svg-icons';
 import {
   faBox,
   faWaveSquare,
   faClock,
+  faAngleUp,
+  faAngleDown,
+  faHeart,
+  faBorderAll,
+  faUsers,
+  faGear,
 } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-boards',
   standalone: true,
-  imports: [NavbarComponent, FontAwesomeModule],
+  imports: [
+    CommonModule,
+    NavbarComponent,
+    FontAwesomeModule,
+    CdkAccordionModule,
+  ],
   templateUrl: './boards.component.html',
 })
 export class BoardsComponent {
@@ -20,4 +33,10 @@ export class BoardsComponent {
   faBox = faBox;
   faWaveSquare = faWaveSquare;
   faClock = faClock;
+  faAngleUp = faAngleUp;
+  faAngleDown = faAngleDown;
+  faHeart = faHeart;
+  faBorderAll = faBorderAll;
+  faUsers = faUsers;
+  faGear = faGear;
 }
