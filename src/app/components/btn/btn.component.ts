bd926c52ad71e4acfd440991a1ae7ed6d096141d
AdrianGerman <adrian.german1019@gmail.com>
2024-04-02 14:25:40 -0600

    todo dialog design

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -11,8 +11,12 @@ export class BtnComponent {
   @Input() typeBtn: 'button' | 'reset' | 'submit' = 'button';
   @Input() color = 'primary';
 
+  // gray-light
+
   get colors() {
     return {
+      'text-white': this.color === 'success' || this.color === 'primary',
+      'text-gray-700': this.color === 'gray-light',
       'bg-success-600': this.color === 'success',
       'hover:bg-success-800': this.color === 'success',
       'focus:ring-success-300': this.color === 'success',
@@ -22,6 +26,9 @@ export class BtnComponent {
       'bg-blue-600': this.color === 'sky',
       'hover:bg-blue-800': this.color === 'sky',
       'focus:ring-blue-300': this.color === 'sky',
+      'bg-gray-200': this.color === 'gray-light',
+      'hover:bg-gray-500': this.color === 'gray-light',
+      'focus:ring-gray-50': this.color === 'gray-light',
     };
   }
 }
--- src/app/components/todo-dialog/todo-dialog.component.ts
+++ src/app/components/todo-dialog/todo-dialog.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import {
+  faClose,
+  faCheckToSlot,
+  faBars,
+  faUser,
+  faTag,
+  faCheckSquare,
+  faClock,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BtnComponent } from '../btn/btn.component';
+import { DialogRef } from '@angular/cdk/dialog';
 
 @Component({
   selector: 'app-todo-dialog',
   standalone: true,
-  imports: [],
-  templateUrl: './todo-dialog.component.html'
+  imports: [FontAwesomeModule, BtnComponent],
+  templateUrl: './todo-dialog.component.html',
 })
 export class TodoDialogComponent {
+  // icons
+  faClose = faClose;
+  faCheckToSlot = faCheckToSlot;
+  faBars = faBars;
+  faUser = faUser;
+  faTag = faTag;
+  faCheckSquare = faCheckSquare;
+  faClock = faClock;
 
+  constructor(private dialogRef: DialogRef) {}
+
+  close() {
+    this.dialogRef.close();
+  }
 }
