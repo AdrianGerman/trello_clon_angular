fcace32ab845ccca123698cb7c55b8eca208abea
AdrianGerman <adrian.german1019@gmail.com>
2024-04-04 10:29:44 -0600

    fix: button and todo-dialog

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -9,14 +9,50 @@ import { Component, Input } from '@angular/core';
 })
 export class BtnComponent {
   @Input() typeBtn: 'button' | 'reset' | 'submit' = 'button';
-  @Input() color = 'primary';
+  @Input() color: 'success' | 'primary' | 'danger' | 'light' | 'sky' =
+    'primary';
+
+  mapColors = {
+    success: {
+      'bg-success-600': true,
+      'hover:bg-success-800': true,
+      'focus:ring-success-300': true,
+      'text-white': true,
+    },
+    primary: {
+      'bg-primary-600': true,
+      'hover:bg-primary-800': true,
+      'focus:ring-primary-300': true,
+      'text-white': true,
+    },
+    sky: {
+      'bg-sky-600': true,
+      'hover:bg-sky-800': true,
+      'focus:ring-sky-300': true,
+      'text-white': true,
+    },
+    danger: {
+      'bg-danger-600': true,
+      'hover:bg-danger-800': true,
+      'focus:ring-danger-300': true,
+      'text-white': true,
+    },
+    light: {
+      'bg-gray-200': true,
+      'hover:bg-gray-400': true,
+      'focus:ring-gray-50': true,
+      'text-gray-700': true,
+    },
+  };
 
   // gray-light
 
   get colors() {
+    const colors = this.mapColors[this.color];
+    if (colors) {
+      return colors;
+    }
     return {
-      'text-white': this.color === 'success' || this.color === 'primary',
-      'text-gray-700': this.color === 'gray-light',
       'bg-success-600': this.color === 'success',
       'hover:bg-success-800': this.color === 'success',
       'focus:ring-success-300': this.color === 'success',
@@ -26,9 +62,9 @@ export class BtnComponent {
       'bg-blue-600': this.color === 'sky',
       'hover:bg-blue-800': this.color === 'sky',
       'focus:ring-blue-300': this.color === 'sky',
-      'bg-gray-200': this.color === 'gray-light',
-      'hover:bg-gray-500': this.color === 'gray-light',
-      'focus:ring-gray-50': this.color === 'gray-light',
+      'bg-gray-200': this.color === 'light',
+      'hover:bg-gray-500': this.color === 'light',
+      'focus:ring-gray-50': this.color === 'light',
     };
   }
 }
