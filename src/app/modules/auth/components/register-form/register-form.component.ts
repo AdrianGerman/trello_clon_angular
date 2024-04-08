660763561dc3eafe7012a5b27c30a2755c27da4b
AdrianGerman <adrian.german1019@gmail.com>
2024-04-08 10:00:54 -0600

    optimizing registration flow

--- src/app/modules/auth/components/register-form/register-form.component.ts
+++ src/app/modules/auth/components/register-form/register-form.component.ts
@@ -49,7 +49,7 @@ export class RegisterFormComponent {
       this.authService.register(name, email, password).subscribe({
         next: () => {
           this.status = 'success';
-          this.router.navigate(['/login']);
+          this.router.navigate(['/app/boards']);
         },
         error: () => {
           this.status = 'failed';
