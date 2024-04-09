ea8c64ab7a46e88a719bb37b07f0cd21816456c1
AdrianGerman <adrian.german1019@gmail.com>
2024-04-09 11:53:05 -0600

    reactivity in profile

--- src/app/modules/users/pages/users-table/users-table.component.ts
+++ src/app/modules/users/pages/users-table/users-table.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { DataSourceUser } from './data-source';
 import { UsersService } from '@services/users.service';
 
+import { AuthService } from '@services/auth.service';
+import { User } from '@models/user.model';
+
 @Component({
   selector: 'app-users-table',
   templateUrl: './users-table.component.html',
@@ -10,12 +13,19 @@ import { UsersService } from '@services/users.service';
 export class UsersTableComponent implements OnInit {
   dataSource = new DataSourceUser();
   columns: string[] = ['id', 'avatar', 'name', 'email'];
+  user: User | null = null;
 
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.userService.getUsers().subscribe((users) => {
       this.dataSource.init(users);
+    });
+    this.authService.user$.subscribe((user) => {
+      this.user = user;
     });
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,13 +5,19 @@ import { switchMap, tap } from 'rxjs';
 import { TokenService } from './token.service';
 import { ResponseLogin } from '@models/auth.model';
 import { User } from '@models/user.model';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   apiUrl = environment.API_URL;
+  user$ = new BehaviorSubject<User | null>(null);
   constructor(private http: HttpClient, private tokenService: TokenService) {}
+
+  getDataUser() {
+    return this.user$.getValue();
+  }
 
   login(email: string, password: string) {
     return this.http
@@ -59,11 +65,17 @@ export class AuthService {
 
   getProfile() {
     const token = this.tokenService.getToken();
-    return this.http.get<User>(`${this.apiUrl}/api/v1/auth/profile`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+    return this.http
+      .get<User>(`${this.apiUrl}/api/v1/auth/profile`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .pipe(
+        tap((user) => {
+          this.user$.next(user);
+        })
+      );
   }
 
   logout() {
